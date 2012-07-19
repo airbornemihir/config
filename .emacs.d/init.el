dd45adae420c696b5f5031a89d26512f867a212b
Mihir <mihirthegenius@gmail.com>
2012-07-19 10:09:56 +0530

    Changes in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,8 +5,6 @@
 (require 'jabber)
 (require 'psvn)
 
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-
 (add-hook 'python-mode-hook
   (lambda ()
     (setq python-indent 8)
@@ -21,12 +19,9 @@
 
 (global-linum-mode 1)
 
-(setq-default x-select-enable-clipboard t)
-
 (setq-default menu-bar-games-menu t)
 
-(setq snake-score-file
-    "~/.emacs.d/snake-scores")
+(setq snake-score-file "~/.emacs.d/snake-scores")
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (custom-set-variables
@@ -36,8 +31,13 @@
   ;; If there is more than one, they won't work right.
  '(jabber-account-list (quote (("mihirthegenius@gmail.com" (:network-server . "talk.google.com") (:connection-type . ssl)) ("mihir@mygola.com" (:network-server . "talk.google.com") (:connection-type . ssl)))))
  '(jabber-alert-presence-message-function (lambda (WHO OLDSTATUS NEWSTATUS STATUSTEXT) (nil)))
- '(jabber-sort-order (quote ("chat" "dnd" "away" "xa" "")))
- '(py-python-command "~/mygola/trunk/env/bin/python"))
+ '(jabber-default-show "dnd")
+ '(jabber-default-status "")
+ '(jabber-sort-order (quote ("chat" "" "dnd" "away" "xa")))
+ '(py-python-command "~/mygola/trunk/env/bin/python")
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
+ '(x-select-enable-clipboard t)
+ '(x-select-enable-primary t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
